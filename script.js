d5d7d29e5fa1d4bad2582dc13608423f3743a4a5
teuss562 <mateus33386325@gmail.com>
2025-04-05 00:29:42 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function createHeart() {
+  const heart = document.createElement('div');
+  heart.className = 'heart';
+  heart.style.left = Math.random() * 100 + 'vw';
+  heart.style.animationDuration = (Math.random() * 2 + 3) + 's';
+  document.body.appendChild(heart);
+  setTimeout(() => heart.remove(), 5000);
+}
+
+setInterval(createHeart, 300);
